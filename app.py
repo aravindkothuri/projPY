93bdf56f736025d3ad71d23009362c28f4c17b5f
aravindkothuri <aravindkumar9247@gmail.com>
2025-11-16 14:27:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,6 +69,19 @@ sum = num1 + num2
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
 
+//code-3 starts from here
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+
 
 
 
