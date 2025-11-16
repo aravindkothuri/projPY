3e41e67517b370f3f08b1bea59fe3267d13dd4fc
aravindkothuri <aravindkumar9247@gmail.com>
2025-11-16 14:38:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,30 +55,6 @@ while True:
         print("Invalid Input")
 
 
-//code-2 data ll start from here
-
-# This program adds two numbers
-
-num1 = 1.5
-num2 = 6.3
-
-# Add two numbers
-sum = num1 + num2
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
-
-
-//code-3 starts from here
-# Store input numbers
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
-
-# Add two numbers
-sum = float(num1) + float(num2)
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
 
 
